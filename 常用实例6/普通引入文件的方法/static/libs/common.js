2e3225d8f429d40a3859b9ef6ee2a18f0d37bf36
zhenxiaowei <270774091@qq.com>
2018-11-10 12:34:42 +0800

    ..

---
+++ 常用实例6/普通引入文件的方法/static/libs/common.js
@@ -0,0 +1,7 @@
+document.write('    <script src="static/libs/jquery/jquery.js"></script>\n');
+document.write('    <script src="static/libs/bootstrap/js/bootstrap.js"></script>\n');
+document.write('    <script src="static/libs/echarts/echarts.js"></script>\n');
+document.write('    <script src="static/libs/toastr/toastr.js"></script>\n');
+document.write('    <script src="static/libs/vue/vue.js"></script>\n');
+document.write('    <script src="static/libs/loadingmodal/loadingmodal-min.js"></script>\n');
+document.write('    <script src="static/libs/zxw.js"></script>');
