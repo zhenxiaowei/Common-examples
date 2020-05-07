52e2ac2c94b5c73c89d4db96ea8ff1f220b30d4f
甄晓伟 <270774091@qq.com>
2020-05-07 22:48:56 +0800

    update

--- 常用实例2/datatable/table_demo/js/login.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zhenxiaowei on 2017/8/12.
- */
-$(".submit").click(function(){
-    window.location.href="index.html";
-})
--- 常用实例5/vue2.0/vue-datepicker/datepicker-local-demo/require/static/mm.js
+++
@@ -1,9 +0,0 @@
-define(['vue', 'VueDatepickerLocal'], function (Vue) {
-    new Vue({
-        el: "#app",
-        data: {
-            time: ''
-        }
-
-    })
-})
